f4d6816afbb037215714221f6952551cada306b9
sofencyXiao <sofency@qq.com>
2019-07-06 22:50:42 +0800

    stream

---
+++ JAVA/java基础学习2/src/IO学习开篇/src/stream/ImageStream.java
@@ -0,0 +1,46 @@
+package stream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 1.第一个程序；理解操作步骤
+ * @author sofency
+ * 1.创建源
+ * 2.选择流
+ * 3.操作
+ * 4.释放资源
+ */
+public class ImageStream {
+	public static void main(String[] args) throws FileNotFoundException {
+		//创建源
+		File src=new File("test.txt");//
+		//选择源
+		InputStream  is=null;
+		//操作
+		int temp;
+		try {
+			is=new FileInputStream(src);
+			while((temp=is.read())!=-1) {//先将is.read()赋值给temp 然后temp和-1进行比较
+				System.out.print((char)temp);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				if(null!=is) {
+					is.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
