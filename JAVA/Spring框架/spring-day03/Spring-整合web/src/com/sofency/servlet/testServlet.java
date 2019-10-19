2c9fac8cfe17f88bf9b61f4ef11887e448757e0f
sofencyXiao <sofency@qq.com>
2019-10-19 22:59:58 +0800

    spring整合web

---
+++ JAVA/Spring框架/spring-day03/Spring-整合web/src/com/sofency/servlet/testServlet.java
@@ -0,0 +1,32 @@
+package com.sofency.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.sofency.transferService.transfer;
+
+/**
+ * Servlet implementation class testServlet
+ */
+public class testServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ApplicationContext applicationContext=
+				(ApplicationContext)this.getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+		
+		transfer transfer = (transfer)applicationContext.getBean("transferServiceId");
+		transfer.transferMoney("jack", "rose", 1000);  
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
